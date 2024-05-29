83a02e3efaa947e0ec7359677fbf44c0b7f47741
Gabriele De Bona <118283485+debonabbo@users.noreply.github.com>
2024-05-29 15:30:25 +0200

    confondo sempre sto carattere boh

--- prova_esame/compare.c
+++ prova_esame/compare.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 _Bool str_equal(char* s1, char* s2){
-    while(*s1 == *s2 && *s1 != '\n' && *s2 != '\n') { s1++; s2++; }
+    while(*s1 == *s2 && *s1 != '\0' && *s2 != '\0') { s1++; s2++; }
     return (*s1 == *s2);
 }
 
