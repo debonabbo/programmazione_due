8d12076af87ef6793eea2b1169e94912fc57a103
Gabriele De Bona <118283485+debonabbo@users.noreply.github.com>
2024-06-03 23:04:17 +0200

    documentazione in doxygen

--- 4_progetto_quattro/charQueueADT.h
+++ 4_progetto_quattro/charQueueADT.h
@@ -1,23 +1,52 @@
 /* Un tipo di dato astratto per le code di char */
 typedef struct charQueue *CharQueueADT;
 
-/* @brief Restituisce una coda vuota, se non trova memoria restituisce NULL */
+/**
+ * @brief Crea una coda vuota
+ * @return La coda creata, 0 se non c'e' memoria sufficiente 
+*/
 CharQueueADT mkQueue();
 
-/* @brief Distrugge la coda, recuperando la memoria */
+/**
+ * @brief Distrugge la coda, recuperando la memoria
+ * @param 
+*/
 void dsQueue(CharQueueADT *pq);
 
-/* @brief Aggiunge un elemento in fondo alla coda, restituisce esito 0/1 */
+/**
+ * @brief Aggiunge un elemento in fondo alla coda
+ * @return 1 se l'operazione e' andata a buon fine, 0 altrimenti 
+*/
 _Bool enqueue(CharQueueADT q, const char e);
 
-/* @brief Toglie e restituisce l'elemento in testa alla coda, restituisce esito 0/1 */
+/**
+ * @brief Toglie e restituisce l'elemento in testa alla coda
+ * @param q la coda da cui togliere l'elemento
+ * @param res* il puntatore che conterra' l'elemento rimosso
+ * @return 1 se l'operazione e' andata a buon fine, 0 altrimenti
+*/
 _Bool dequeue(CharQueueADT q, char*res);
 
-/* @brief Controlla se la coda è vuota */
+/** 
+ * @brief Controlla se la coda è vuota 
+ * @param q la coda da controllare
+ * @return 1 se la coda e' vuota, 0 altrimenti
+*/
 _Bool isEmpty(CharQueueADT q);
 
-/* @brief Restituisce il numero degli elementi presenti nella coda */
+/**
+ * @brief Restituisce il numero degli elementi presenti nella coda
+ * @param q la coda
+ * @return Il numero di elementi nella coda
+*/
 int size(CharQueueADT q);
 
-/* @brief Restituisce l'elemento nella posizione data (a partire dalla testa con indice zero) (senza toglierlo), restituisce esito 0/1 */
+/**
+ * @brief Restituisce l'elemento nella posizione data senza toglierlo.
+ * La testa della coda ha indice 0.
+ * @param q la coda in cui prendere l'elemento
+ * @param position la posizione in cui prendere l'elemento
+ * @param res* il puntatore che conterra' l'elemento richiesto
+ * @return 1 se l'operazione e' andata a buon fine, 0 altrimenti
+*/
 _Bool peek(CharQueueADT q, int position, char* res);
--- 5_progetto_cinque/intSetADT.h
+++ 5_progetto_cinque/intSetADT.h
@@ -8,22 +8,116 @@ typedef struct intSet *IntSetADT;
 /*
  NOTA:
  le seguenti funzioni, in base al loro tipo, devono restituire 
- NULL, -1 oppure false (0) se ricevono uno o più puntatori NULL come parametri di tipo IntSetADT
+ NULL, -1 oppure false (0) se ricevono uno o più puntatori NULL come 
+ parametri di tipo IntSetADT
 */
 
-IntSetADT mkSet(); // restituisce un insieme vuoto
-_Bool dsSet(IntSetADT*); // distrugge l'insieme (inclusi i suoi elementi), recuperando la memoria
-_Bool set_add(IntSetADT, const int); // aggiunge un elemento all'insieme (restituisce 0 se l'elemento era gia' presente, 1 altrimenti)
-_Bool set_remove(IntSetADT, const int); // toglie un elemento all'insieme (restituisce 0 se l'elemento non era presente, 1 altrimenti)
-_Bool set_member(const IntSetADT, const int); // controlla se un elemento appartiene all'insieme
-_Bool isEmptySet(const IntSetADT); // controlla se l'insieme e' vuoto
-int set_size(const IntSetADT); // restituisce il numero di elementi presenti nell'insieme, -1 se NULL
-_Bool set_extract(IntSetADT, int *); // toglie e restituisce un elemento a caso dall'insieme
-_Bool set_equals(const IntSetADT, const IntSetADT); // controlla se due insiemi sono uguali
-_Bool subseteq(const IntSetADT, const IntSetADT); // controlla se il primo insieme e' incluso nel secondo
-_Bool subset(const IntSetADT, const IntSetADT); // contr. se il primo ins. e' incluso strettamente nel secondo
-IntSetADT set_union(const IntSetADT, const IntSetADT); // restituisce l'unione di due insiemi
-IntSetADT set_intersection(const IntSetADT, const IntSetADT); // restituisce l'intersezione di due insiemi
-IntSetADT set_subtraction(const IntSetADT, const IntSetADT); // restituisce l’insieme ottenuto dal primo togliendo gli elementi del secondo
+/**
+ * @brief Costruisce un insieme vuoto
+ * @return Un insieme vuoto
+*/
+IntSetADT mkSet();
+
+/**
+ * @brief Distrugge l'insieme (e tutti i suoi elementi) recuperando la memoria
+ * @param sp* il puntatore al set da distruggere
+ * @return 1 se l'operazione e' stata completata, 0 altrimenti
+*/
+_Bool dsSet(IntSetADT*);
+
+/**
+ * @brief Aggiunge un elemento all'insieme
+ * @param set il set a cui aggiungere l'elemento
+ * @param elem l'elemento da aggiungere
+ * @return 0 se l'elemento era gia' presente, 1 altrimenti
+*/
+_Bool set_add(IntSetADT, const int);
+
+/**
+ * @brief Toglie un elemento dall'insieme
+ * @param set il set da cui rimuovere l'elemento
+ * @param int l'elemento da rimuovere
+ * @return 1 se l'elemento era presente ed e' stato rimosso, 0 altrimenti
+*/
+_Bool set_remove(IntSetADT, const int);
+
+/**
+ * @brief Controlla se un elemento appartiene all'insieme
+ * @param set il set in cui cercare l'elemento
+ * @param int l'elemento da cercare
+ * @return 1 se l'elemento appartiene all'insieme, 0 altrimenti
+*/
+_Bool set_member(const IntSetADT, const int);
+
+/**
+ * @brief Controlla se l'insieme e' vuoto
+ * @param set il set da controllare 
+ * @return 1 se l'insieme e' vuoto, 0 altrimenti
+*/
+_Bool isEmptySet(const IntSetADT);
+
+/**
+ * @brief Restituisce il numeri di elementi presenti nell'insieme
+ * @param set il set in cui contare gli elementi
+ * @return Il numero di elementi presenti nell'insieme, -1 se il set e' NULL
+*/
+int set_size(const IntSetADT);
+
+/**
+ * @brief Toglie e restituisce un elemento a caso dall'insieme
+ * @param set il set da cui togliere l'elemento
+ * @param datap* il puntatore che conterra' l'elemento tolto
+ * @return 1 se l'operazione e' stata completata, 0 altrimenti
+*/
+_Bool set_extract(IntSetADT, int *);
+
+/**
+ * @brief Controlla se due insiemi sono uguali
+ * @param set1 il primo insieme da controllare
+ * @param set2 il secondo insieme da controllare
+ * @return 1 se gli insiemi sono uguali, 0 altrimenti
+*/
+_Bool set_equals(const IntSetADT, const IntSetADT);
+
+/**
+ * @brief Controlla se il primo insieme e' incluso o coincide col secondo
+ * @param set1 il primo insieme
+ * @param set2 il secondo insieme
+ * @return 1 se il primo insieme e' incluso o coincide col secondo, 0 altrimenti
+*/
+_Bool subseteq(const IntSetADT, const IntSetADT);
+
+/**
+ * @brief Controlla se il primo insieme e' incluso strettamente nel secondo
+ * @param set1 il primo insieme
+ * @param set2 il secondo insieme
+ * @return 1 se il primo insieme e' incluso strettamente nel secondo,
+ * 0 altrimenti
+*/
+_Bool subset(const IntSetADT, const IntSetADT);
+
+/**
+ * @brief Restituisce l'unione di due insiemi
+ * @param set1 il primo insieme
+ * @param set2 il secondo insieme
+ * @return Un nuovo insieme contenente l'unione dei due insiemi
+*/
+IntSetADT set_union(const IntSetADT, const IntSetADT);
+
+/**
+ * @brief Restituisce l'intersezione di due insiemi
+ * @param set1 il primo insieme
+ * @param set2 il secondo insieme
+ * @return Un nuovo insieme contenente l'intersezione dei due insiemi
+*/
+IntSetADT set_intersection(const IntSetADT, const IntSetADT);
+
+/**
+ * @brief Restituisce la sottrazione di due insiemi
+ * @param set1 il primo insieme
+ * @param set2 il secondo insieme
+ * @return Un nuovo insieme contenente la sottrazione dei due insiemi
+*/
+IntSetADT set_subtraction(const IntSetADT, const IntSetADT);
 
 #endif
